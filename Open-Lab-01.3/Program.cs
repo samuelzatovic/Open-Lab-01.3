1d3e65f84783773efc69fbca4d1480bb5392af10
samuel zaťovič <szatovic@gmail.com>
2019-10-13 14:44:34 +0200

    Program

---
+++ Open-Lab-01.3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Open_Lab_01._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
